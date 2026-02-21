27382d10b0d52725a60d3c4f1560beb695cf8d70
wdabek85 <wdabek85@gmail.com>
2026-02-21 13:18:18 +0100

    top hero skonczone

--- bedrock/web/app/themes/moj-motyw/app/filters.php
+++ bedrock/web/app/themes/moj-motyw/app/filters.php
@@ -14,3 +14,13 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+
+add_filter('acf/settings/save_json', function ($path) {
+    return get_stylesheet_directory() . '/resources/acf-json';
+});
+
+add_filter('acf/settings/load_json', function ($paths) {
+    $paths[] = get_stylesheet_directory() . '/resources/acf-json';
+    return $paths;
+});
--- bedrock/web/app/themes/moj-motyw/resources/views/sections/hero/top-hero.blade.php
+++ bedrock/web/app/themes/moj-motyw/resources/views/sections/hero/top-hero.blade.php
@@ -1,7 +1,15 @@
-<div class="bg-gray-900 text-white">
-    <div class="max-w-[1440px] m-auto">
-        <h1>{{ $sekcjeHero['tytul']}}</h1>
-        <p>{{$sekcjeHero['podtytul']}}</p>
+<div class="bg-primary-400 text-white">
+    <div class="w-full py-6 px-5 lg:max-w-[1440px] m-auto flex flex-col gap-6 items-center lg:flex-row  lg:pl-20">
+        <div class="flex gap-4 flex-col">
+            <h1 class="display-md-medium">{{ $sekcjeHero['tytul']}}</h1>
+            <p class="text-md-regular">{{$sekcjeHero['podtytul']}}</p>
+            <x-button variant="outline" size='md' class="bg-orange-400 w-fit"> 
+                <span>Dowiedz się o nas Więcej</span>
+                 <x-slot:iconAfter>
+                    <x-icons.arrow />
+                </x-slot:iconAfter>
+            </x-button>
+        </div>
         @if($sekcjeHero['image'])
             <img src="{{ $sekcjeHero['image']['url'] }}" alt="{{ $sekcjeHero['image']['alt'] }}">
         @endif
